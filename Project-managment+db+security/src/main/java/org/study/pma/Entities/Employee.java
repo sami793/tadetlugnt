0e3d0c0e037374ef022ff849564cfe82f05bbdc4
sami_ <sami_@DESKTOP-O6B42TT>
2021-01-12 20:34:56 +0100

    4th commit

---
+++ Project-managment+db+security/src/main/java/org/study/pma/Entities/Employee.java
@@ -0,0 +1,147 @@
+package org.study.pma.Entities;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+
+
+
+@Entity
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator="employee_seq")
+    @SequenceGenerator(name="employee_seq",sequenceName="employee_seq", allocationSize = 1)
+
+	private long employeeId;
+	
+	private String firstName;
+		
+	private String lastName;
+	
+	private String email;
+
+	
+	@ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.PERSIST},
+			   fetch = FetchType.LAZY)
+	@JoinTable(name="project_employee",
+	joinColumns=@JoinColumn(name="employee_id"),
+	inverseJoinColumns= @JoinColumn(name="project_id"))
+	
+	private List<Project> projects;
+
+	
+	
+
+	public Employee() {
+		super();
+	}
+
+
+
+
+	public Employee(String firstName, String lastName, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+
+
+
+	public long getEmployeeId() {
+		return employeeId;
+	}
+
+
+
+
+	public void setEmployeeId(long employeeId) {
+		this.employeeId = employeeId;
+	}
+
+
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+
+	public List<Project> getProjects() {
+		return projects;
+	}
+
+
+
+
+	public void setProjects(List<Project> projects) {
+		this.projects = projects;
+	}
+
+
+
+
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", firstName=" + firstName + ", lastName=" + lastName + ", email="
+				+ email + ", projects=" + projects + "]";
+	}
+
+
+
+
+
+	
+	
+	
+	
+	
+}
